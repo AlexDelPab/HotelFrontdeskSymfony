325c31436a30a23d82720eb92059a394c76f4f77
AlexDelPab <a.pabinger@gmx.at>
2018-12-30 20:21:10 +0100

    Add stylesheets

--- HotelFrontdesk/src/AppBundle/Controller/DefaultController.php
+++ HotelFrontdesk/src/AppBundle/Controller/DefaultController.php
@@ -3,19 +3,14 @@
 namespace AppBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
-use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 
-class DefaultController extends Controller
-{
+class DefaultController extends Controller {
+
     /**
-     * @Route("/", name="homepage")
+     * @Route("/", name="index")
      */
-    public function indexAction(Request $request)
-    {
-        // replace this example code with whatever you need
-        return $this->render('default/index.html.twig', [
-            'base_dir' => realpath($this->getParameter('kernel.project_dir')).DIRECTORY_SEPARATOR,
-        ]);
+    public function indexAction() {
+        return $this->render('@App/index.html.twig');
     }
 }
--- src/HotelFrontdesk/AppBundle/AppBundle.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-
-namespace App\HotelFrontdesk\AppBundle;
-
-
-class AppBundle {
-
-}
