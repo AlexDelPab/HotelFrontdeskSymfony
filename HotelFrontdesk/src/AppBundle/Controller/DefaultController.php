02a167a0177e45f0235e2e9262a747f10cb90a5f
AlexDelPab <a.pabinger@gmx.at>
2018-12-30 20:46:59 +0100

    Change all hrefs and create other routes

--- HotelFrontdesk/src/AppBundle/Controller/DefaultController.php
+++ HotelFrontdesk/src/AppBundle/Controller/DefaultController.php
@@ -13,4 +13,46 @@ class DefaultController extends Controller {
     public function indexAction() {
         return $this->render('@App/index.html.twig');
     }
+
+    /**
+     * @Route("/blog", name="blog")
+     */
+    public function blogAction() {
+        return $this->render('@App/blog.html.twig');
+    }
+
+    /**
+     * @Route("/blogSingle", name="blog_single")
+     */
+    public function blogSingleAction() {
+        return $this->render('@App/blog.single.html.twig');
+    }
+
+    /**
+     * @Route("/about", name="about")
+     */
+    public function aboutAction() {
+        return $this->render('@App/about.html.twig');
+    }
+
+    /**
+     * @Route("/contact", name="contact")
+     */
+    public function contactAction() {
+        return $this->render('@App/contact.html.twig');
+    }
+
+    /**
+     * @Route("/booknow", name="booknow")
+     */
+    public function booknowAction() {
+        return $this->render('@App/booknow.html.twig');
+    }
+
+    /**
+     * @Route("/rooms", name="rooms")
+     */
+    public function roomsAction() {
+        return $this->render('@App/rooms.html.twig');
+    }
 }
