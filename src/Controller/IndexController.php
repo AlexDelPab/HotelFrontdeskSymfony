d1aaf333390a6cd9581945c9d50bc9b54b94c420
AlexDelPab <a.pabinger@gmx.at>
2018-12-30 15:32:31 +0100

    Initial commit

---
+++ src/Controller/IndexController.php
@@ -0,0 +1,18 @@
+<?php
+
+
+namespace App\Controller;
+
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\Routing\Annotation\Route;
+
+class IndexController extends Controller {
+
+    /**
+     * @Route("/", name="index")
+     */
+    public function indexAction() {
+        return $this->render('base.html.twig');
+    }
+}
